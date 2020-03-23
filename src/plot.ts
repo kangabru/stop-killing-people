6f7d0b7a2a2a2d0e21754c2d12c027d04de03273
Scott Gibbs <scott@gibbz.biz>
2020-03-22 18:50:05 -0600

    Simplified code by moving non-dynamic code out of the update function. Also added axis labels and fixed the title position.

--- src/plot.ts
+++ src/plot.ts
@@ -1,21 +1,58 @@
 import * as d3 from 'd3';
 // @ts-ignore
 import d3tip from 'd3-tip';
-import get_data from './data'
-import { WorldData } from './types'
+import get_data from './data';
+import { WorldData } from './types';
 
-const margin = { top: 20, right: 20, bottom: 30, left: 20 }
+const margin = { top: 20, right: 20, bottom: 70, left: 50, text: 5 }
 const height = 400, width = 600, delay = 1000
+const heightPlot = height - margin.top - margin.bottom,
+    widthPlot = width - margin.left - margin.right
 
 const create_chart = (world: WorldData) => {
     const dates = world.dates
+    let barWidth = width / dates.length
 
-    let chart = d3.select('svg')
+    const svg = d3.select('svg')
         .attr("height", height)
         .attr("width", width)
-        .append('g')
+
+    const title = svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", margin.top)
+        .attr("text-anchor", "middle")
+
+    svg.append("text")
+        .attr("x", margin.left)
+        .attr("y", margin.top)
+        .attr("text-anchor", "end")
+        .text("Cases")
+
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height - margin.top + 6)
+        .attr("text-anchor", "middle")
+        .text("Days")
+
+    const chart = svg.append('g')
         .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
+    const y = d3.scaleLinear()
+        .range([height - margin.bottom, margin.top])
+
+    const x = d3.scaleLinear()
+        .domain([0, dates.length])
+        .range([0, widthPlot])
+
+    const xAxis = chart.append("g")
+        .attr("class", "axis x")
+        .attr("y", 100)
+        .attr("transform", `translate(0, ${margin.top + heightPlot})`)
+
+    const yAxis = chart.append("g")
+        .attr("class", "axis y")
+
+    const t = chart.transition().duration(delay)
 
     let tip = d3tip()
         .attr('class', 'd3-tip')
@@ -27,57 +64,32 @@ const create_chart = (world: WorldData) => {
             const date_pretty = `${da}-${mo}-${ye}`
             return `<strong>${num} cases</strong><br>${date_pretty}`
         })
-
     chart.call(tip)
-
-    const t = chart.transition().duration(delay);
-    const title = chart.append("text").attr("x", 100)
 
     return (country: string) => {
         const data = world.countries[country][0]
 
-        let y = d3.scaleLinear()
-            .domain([0, d3.max(data.cases)]).nice()
-            .range([height - margin.bottom, margin.top])
+        y.domain([0, d3.max(data.cases)]).nice()
 
-        let x = d3.scaleLinear()
-            .domain([0, dates.length])
-            .range([0, width])
+        xAxis.call(d3.axisBottom(x).ticks(10))
+        yAxis.call(d3.axisLeft(y).ticks(10))
 
-        let barWidth = width / world.dates.length
+        title.text(`${data.state && (data.state + ", ")}${data.country}`)
 
-        chart.append("g")
-            .attr("class", "axis x")
-            .attr("transform", `translate(0,${height})`)
-            .call(d3.axisBottom(x).ticks(10));
-
-        chart.append("g")
-            .attr("class", "axis y")
-            .call(d3.axisLeft(y).ticks(10))
-
-        title.text(`${data.state && (data.state + ", ")}${data.country}`);
-
-        const rect = chart.selectAll('.bar')
+        chart.selectAll('.bar')
             .data(data.cases)
-            .join(
-                enter =>
-                    enter.append('rect')
-                        .attr('class', 'bar')
-                        .attr('x', (_, i) => x(i))
-                        .attr('y', d => y(d))
-                        .attr("height", d => height - margin.bottom - y(d))
-                        .attr("width", barWidth - 2)
-                        .on('mouseover', tip.show)
-                        .on('mouseout', tip.hide),
-                update => update,
-                exit => exit.call(rect => rect.transition(t as any).remove()
-                    .attr("y", y(0))
-                    .attr("height", 0))
-            )
-
-        rect.transition(t as any)
+            .join(enter => enter
+                .append('rect')
+                .attr('class', 'bar')
+                .attr('x', (_, i) => x(i))
+                .attr('y', d => y(d))
+                .attr("height", d => height - margin.bottom - y(d))
+                .attr("width", barWidth - 2)
+                .on('mouseover', tip.show)
+                .on('mouseout', tip.hide))
+            .transition(t as any)
             .attr("y", d => y(d))
-            .attr("height", d => y(0) - y(d));
+            .attr("height", d => y(0) - y(d))
     }
 }
 
